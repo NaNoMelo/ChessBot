85031b22aa055f22b27d4b30519ee3c2ff5a1a51
NaNo Melo <nathan.lamey68@orange.fr>
2022-03-24 14:43:05 +0100

    giteub

---
+++ bot.js
@@ -0,0 +1,29 @@
+const { Client, Intents } = require("discord.js");
+const config = require("./config.json");
+
+const bot = new Client({intents:[Intents.FLAGS.GUILDS]});
+
+bot.on("ready", () => {
+    console.log('Je suis pr\xEAt \xE0 \xEAtre utilis\xE9 !, '+ bot.user.tag);
+});
+
+bot.on("messageCreate", async (message) => {
+    console.log(message.content);
+	/*if (message.content.startsWith(config.prefix) && !message.author.bot ) {
+
+		const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+
+		switch (command) {
+            case "ping":
+                message.reply("Pong!");
+            break;
+        }
+    }*/
+
+    if (message.content() == "ping") {
+        message.reply("Pong");
+    }
+})
+
+bot.login(config.token)
