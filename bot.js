4cf42970f338c57458faf73ea14fd0f995582e1a
NaNo Melo <nathan.lamey68@orange.fr>
2022-04-24 21:55:07 +0200

    Implemented game generator and handling

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require("discord.js")
+const { Client, Intents, Formatters } = require("discord.js")
 const { createCanvas, loadImage, Image, Canvas } = require("canvas")
 const config = require("./config.json")
 const fs = require("fs")
@@ -13,17 +13,8 @@ class game {
     constructor(j1ID, j2ID) {
         this.j1 = j1ID
         this.j2 = j2ID
-        this.boardj1 = games.default
-        this.boardj2 = this.boardj1
+        this.board = games.default
         this.turns = 0
-        this.updatej2()
-    }
-    updatej2() {
-        for (let x = 0; x < 8; x++) {
-            for (let y = 0; y < 8; y++) {
-                this.boardj2[y][x] = this.boardj1[7 - y][7 - x]
-            }
-        }
     }
 }
 
@@ -51,10 +42,10 @@ bot.on("messageCreate", async (message) => {
 
             case "test":
                 games.games[0].j1 = 0
-                console.log(games.games[0].j1)
+                console.log(games.default)
                 break
 
-            case "game":
+            case "challenge":
                 let gameID
                 let player
                 for (let i = 0; i < games.games.length; i++) {
@@ -72,39 +63,47 @@ bot.on("messageCreate", async (message) => {
                         message.reply("Merci de mentionner la personne à défier")
                     } else if (message.mentions.users.first() == undefined) {
                         message.reply("Mention invalide")
-                    } else if (message.mentions.users.first().id == undefined) {
+                    } else if (message.mentions.users.first().id == undefined) { //message.author.id
                         message.reply("Vous ne pouvez pas vous défier vous même")
                     } else if (message.mentions.users.first().bot || message.mentions.users.first().system) {
                         message.reply("Vous ne pouvez pas défier un bot")
                     } else {
                         if (Math.random() < 0.5) {
                             test = new game(message.author.id, message.mentions.users.first().id)
+                            player = 1
                         } else {
                             test = new game(message.mentions.users.first().id, message.author.id)
+                            player = 2
                         }
-                        console.log(games.games.push(test))
-                        console.log(test)
+                        gameID = games.games.push(test) - 1
+
+                        generateBoard(gameID, 1)
+
+                        message.channel.send({
+                            content: `${Formatters.userMention(games.games[gameID].j1)}`,
+                            files: [
+                                {
+                                    attachment: `./boards/board${gameID}_j${1}.png`,
+                                    name: "board.png",
+                                    description: "Plateau de Jeu"
+                                }
+                            ]
+                        })
                     }
+                } else {
+                    let opponent 
+                    if (player == 1) {
+                        opponent = Formatters.userMention(games.games[gameID].j2)
+                    } else {
+                        opponent = Formatters.userMention(games.games[gameID].j1)
+                    }
+                    message.reply(`Vous avez encore une partie en cours contre ${opponent}`)
                 }
+
+                fs.writeFileSync("./games.json", JSON.stringify(games, null, 4))
                 break
         }
     }
-
-    if (message.content == "board") {
-        generateBoard(message.author.id)
-
-        message.channel.send({
-            files: [
-                {
-                    attachment: `./boards/board${message.author.id}.png`,
-                    name: "board.png",
-                    description: "Plateau de Jeu"
-                }
-            ]
-        })
-    }
-
-    //fs.writeFileSync("./games.json",JSON.stringify(games,null,1))
 })
 
 function generateBoard(gameID, player) {
@@ -149,25 +148,34 @@ function generateBoard(gameID, player) {
                 ctx.fillStyle = "black"
             }
 
-            ctx.fillRect(
-                (x + 1) * (board.width / 10),
-                (y + 1) * (board.height / 10),
-                board.width / 10,
-                board.height / 10
-            )
+            ctx.fillRect((x + 1) * (board.width / 10), (y + 1) * (board.height / 10), board.width / 10, board.height / 10)
+        }
+    }
 
-            ctx.drawImage(
-                pieces[games.default[y][x]],
-                (board.width / 10) * (1 + x),
-                (board.height / 10) * (1 + y),
-                board.width / 10,
-                board.height / 10
-            )
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            if (player == 1) {
+                ctx.drawImage(
+                    pieces[games.games[gameID].board[y][x]],
+                    (board.width / 10) * (1 + x),
+                    (board.height / 10) * (1 + y),
+                    board.width / 10,
+                    board.height / 10
+                )
+            } else {
+                ctx.drawImage(
+                    pieces[games.games[gameID].board[y][x]],
+                    (board.width / 10) * (8 - x),
+                    (board.height / 10) * (8 - y),
+                    board.width / 10,
+                    board.height / 10
+                )
+            }
         }
     }
 
     const buffer = board.toBuffer("image/png")
-    fs.writeFileSync(`./boards/board${gameID}.png`, buffer)
+    fs.writeFileSync(`./boards/board${gameID}_j${player}.png`, buffer)
 }
 
 bot.login(process.env.BOT_TOKEN)
